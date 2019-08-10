e5ac96bd368e6d87d5dacc55c82b0d97ce3cad31
pauldev1128 <pauldev1128@gmail.com>
2019-08-09 20:12:23 -0700

    worked on the login.. need to fix bug

--- server/app.js
+++ server/app.js
@@ -78,23 +78,117 @@ app.post('/links',
 // Write your authentication routes here
 /************************************************************/
 
-
+// get request for signup
 app.get("/signup", (req,res) => {
   console.log("you are in the signup page!")
   res.render('signup');
 });
-
+// post request for signup
 app.post("/signup", (req, res) => {
+  // creates username and password
   var username = req.body.username;
   var password = req.body.password;
-  models.Users.create({ username, password})
-    .then(() => res.status(201).send("it worked fam!!!"))
-    .catch((err) => res.status(401).send(err))
-})
 
+  // compare the username with the database
+  // if exists then redirect to signup
+  models.Users.get({ username })
+    .then((userInfo) => {
+      if(!userInfo) {
+        // uses the functions from /models/users create
+        models.Users.create({ username, password})
+        .then(() => res.redirect('/').status(201).send("it worked fam!!!"))
+        .catch((err) => res.status(401).send(err))
+      } else {
+        res.status(201).redirect('/signup')
+      }
+    })
+    .catch((err) => res.status(401).send(err))
+});
+
+
+
+
+// get request for login
 app.get("/login", (req,res) => {
   console.log("you are in the login page!")
   res.render('login');
+});
+
+app.post("/login", (req, res) => {
+  // login username and password
+  var { username, password } = req.body
+  // if (username && password) {
+  //   console.log("test",username)
+  //   console.log("test2",password)
+
+
+
+    // models.Users.getAll({ username, password })
+    //   .then((data) => {
+    //     // console.log("username", username);
+    //     // console.log('pw', password);
+    //     // console.log(data);
+    //     if(username === data.username && password === data.password){
+    //       console.log(data);
+    //       res.redirect('/').status(200).send("message");
+    //     } else {
+    //       res.redirect('/login');
+    //     }
+    //     //compare the entered password with data returned from line 125?
+    //   })
+    //   .catch((err) => {
+    //     console.log('hits .catch with error', err);
+    //   })
+    //   // .then(() => res.redirect('/').status(201).send("you have logged in"))
+    //   // .catch((err) => res.status(401).send(err))
+
+
+      models.Users.get({ username })
+      .then((userInfo) => {
+        console.log('username', username)
+        console.log("password", password)
+        // console.log("ui pw", userInfo.password)
+        // console.log("salt", salt)
+        console.log("ui", userInfo);
+        if (userInfo) {
+          console.log('psdlfdsfsdf' ,userInfo)
+          if(models.Users.compare(password, userInfo.password, salt)) {
+            console.log('this is the if statement',password)
+            console.log("salt", salt)
+            res.redirect('/')
+          } else {
+            res.redirect('/login')
+          }
+        } else {
+          console.log("this is the else ")
+          res.redirect('/login')
+        }
+
+      })
+      .catch((err) => res.status(401).send(err))
+
+      // if user exist
+      // if password matches attempt
+      // redirect homepage
+      // else redirect to login page
+    // else if user doesn't exist
+    // redirect to login page
+
+  //     models.Users.get({ username })
+  //     .then((userInfo) => {
+  //       if(!userInfo) {
+  //         // uses the functions from /models/users create
+  //         models.Users.create({ username, password})
+  //         .then(() => res.redirect('/').status(201).send("it worked fam!!!"))
+  //         .catch((err) => res.status(401).send(err))
+  //       } else {
+  //         res.status(201).redirect('/signup')
+  //       }
+  //     })
+  //     .catch((err) => res.status(401).send(err))
+  // });
+
+
 });
 
 
